766680a7023a45044dc3dae5a95b81b53f0cd7f0
Pandor3 <ThatDumbPhilosopher@gmail.com>
2025-01-29 10:21:09 +0100

    Added exercises 6 and 7

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -1,46 +1,74 @@
+// Ceci définit l'interface DirectorInterface
 interface DirectorInterface {
     workFromHome(): string;
     getCoffeeBreak(): string;
     workDirectorTasks(): string;
 }
 
+// Ceci définit l'interface TeacherInterface
 interface TeacherInterface {
     workFromHome(): string;
     getCoffeeBreak(): string;
     workTeacherTasks(): string;
 }
 
+// Ceci créer la classe Director en implémentant l'interface DirectorInterface
 class Director implements DirectorInterface {
     workFromHome(): string {
-        return "Working from Home"    
-    };
+        return "Working from Home";    
+    }
 
     getCoffeeBreak(): string {
-        return "Getting a coffee break"
-    };
+        return "Getting a coffee break";
+    }
 
     workDirectorTasks(): string {
-        return "Getting to director tasks"
-    };
+        return "Getting to director tasks";
+    }
 }
 
+// Ceci créer la classe Teacher en implémentant l'interface TeacherInterface
 class Teacher implements TeacherInterface {
     workFromHome(): string {
-        return "Cannot work from home"
-    };
+        return "Cannot work from home";
+    }
 
     getCoffeeBreak(): string {
-        return "Cannot have a break"
-    };
+        return "Cannot have a break";
+    }
 
     workTeacherTasks(): string {
-        return "Getting to work"
-    };
+        return "Getting to work";
+    }
 }
 
-function createEmployee(salary: number) {
-    if (salary < 500) {
-        return { salary, string: "Teacher"};
-    } 
-    return { salary, string: "Director"};
+// Ceci est la fonction qui permet de vérifier avec le salaire qui est un Director ou un Teacher
+function createEmployee(salary: number | string): Director | Teacher {
+    if (typeof salary === "number" && salary < 500) {
+        return new Teacher();
+    } else {
+        return new Director();
+    }
+}
+
+function isDirector(employee: Director | Teacher): employee is Director {
+    return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher): string {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    } else {
+        return employee.workTeacherTasks();
+    }
+}
+
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === "Math") {
+        return "Teaching Math";
+    } else if (todayClass === "History") {
+        return "Teaching History";
+    }
 }
